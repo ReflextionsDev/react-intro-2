80d7b53d87a3484d4a59d1ab4f917e62ab40d217
ReflextionsDev <reflextionsdev@gmail.com>
2022-03-10 11:27:56 -0500

    react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        Yeet
+      </div>
+    )
+  }
+}
+
+export default App
